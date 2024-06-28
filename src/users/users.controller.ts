474bbcf38ce737184d51afe6602fad97329d267c
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-06-28 16:58:17 +0700

    global post-handler interceptor to transform data for response

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -10,10 +10,13 @@ import {
     Query,
     UseInterceptors,
 } from '@nestjs/common';
+import { Serialize } from '../interceptors/serialize.interceptor';
 import { CreateUserDto } from './dtos/create-user.dto';
 import { UpdateUserDto } from './dtos/update-user.dto';
+import { UserDto } from './dtos/user.dto';
 import { UsersService } from './users.service';
 
+@Serialize(UserDto)
 @Controller('auth')
 export class UsersController {
     constructor(private usersService: UsersService) {
