d79d80f8d2f1466a2377eca375bb848548994636
Phuc Nguyen <phuc.nguyen@blackbook.ai>
2024-07-09 16:22:57 +0700

    create test db, config relevant middleware

--- src/main.ts
+++ src/main.ts
@@ -1,18 +1,11 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipeConfig } from './config/validationpipe.config';
 async function bootstrap() {
     const app = await NestFactory.create(AppModule);
-    app.useGlobalPipes(
-        new ValidationPipe({
-            whitelist: true, // remove all non-defined properties in DTO from the request object
-            forbidNonWhitelisted: true, // throw an error when non-defined properties are in the request object
-            transform: true, // transform the request object to the DTO class object
-            transformOptions: {
-                enableImplicitConversion: true, // implicit convert query params to their respective types
-            },
-        }),
-    );
+
+    // middleware to validate the request body
+    app.useGlobalPipes(new ValidationPipeConfig());
 
     await app.listen(3000);
 }
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -1,9 +1,12 @@
+// the .env.test has to be loaded first, which before the default .evn file in the AppModule
+require('dotenv').config({ path: '.env.test' });
+
 import { INestApplication } from '@nestjs/common';
 import { Test, TestingModule } from '@nestjs/testing';
 import * as request from 'supertest';
+import { ValidationPipeConfig } from '../src/config/validationpipe.config';
 import { AppModule } from './../src/app.module';
-
-describe('AppController (e2e)', () => {
+describe('App Test (e2e)', () => {
     let app: INestApplication;
 
     beforeEach(async () => {
@@ -12,10 +15,32 @@ describe('AppController (e2e)', () => {
         }).compile();
 
         app = moduleFixture.createNestApplication();
+        app.useGlobalPipes(new ValidationPipeConfig());
+
         await app.init();
     });
 
-    it('/ (GET)', async () => {
+    it('/ (GET) Default test', async () => {
         return request.default(app.getHttpServer()).get('/app').expect(200).expect('Hello World!');
     });
+
+    it('handles signup request', async () => {
+        const email = 'b@gmail.com';
+        return request
+            .default(app.getHttpServer())
+            .post('/auth/signup')
+            .send({
+                email: email,
+                password: '1234',
+            })
+            .expect(201)
+            .then((res) => {
+                expect(res.body.id).toBeDefined();
+                expect(res.body.email).toEqual(email);
+            });
+    });
+
+    // afterEach(async () => {
+    //     await app.close();
+    // });
 });
